7dfadc0038c6d2b632d0425233f0ba464f209cc6
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2021-08-31 07:56:05 +0300

    reusable-modal finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <div class="modal-content">
+        <h1>This is a Modal Header</h1>
+        <p>This is a modal message</p>
+      </div>
+    </Modal>
+    <button type="button" role="button" @click="toggleModal">Open Modal</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Modal,
+  },
+
+  setup() {
+    const modalActive = ref(true);
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value;
+    };
+
+    return { modalActive, toggleModal };
+  },
+};
+</script>
+
+
+<style lang="scss">
+.home {
+  background-color: rgba(0, 176, 234, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+
+    h1,
+    p {
+      margin-bottom: 16px;
+    }
+
+    h1 {
+      font-size: 32px;
+    }
+    p {
+      font-size: 18px;
+    }
   }
 }
-</script>
+</style>
